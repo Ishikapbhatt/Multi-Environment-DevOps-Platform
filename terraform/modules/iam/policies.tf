149b18c9ba3c68e05f78b584bc18dc4cc0686c36
Ishika Bhatt <ishikap1998@gmail.com>
2026-01-22 12:48:09 +0530

    Create policies.tf

---
+++ terraform/modules/iam/policies.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
