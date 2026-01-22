24e00cd1696e317a94a1277a0065e48249a80b03
Ishika Bhatt <ishikap1998@gmail.com>
2026-01-22 12:47:34 +0530

    Create main.tf

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
